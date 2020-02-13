46005b503d3316e98030d1172d5e21205ee01542
myeongjin1115 <myeongjin1115@gmail.com>
2020-02-13 15:38:34 +0900

    Frontend Setup

---
+++ src/Styles/GlobalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export default createGlobalStyle`
+    ${reset};
+    * {
+        box-sizing:border-box;
+    }
+    body {
+        background-color:${props => props.theme.bgColor}
+    }
+`;
---
+++ src/Styles/Theme.js
@@ -0,0 +1,5 @@
+export default {
+  bgColor: "#FAFAFA"
+
+  // import default CSS
+};
